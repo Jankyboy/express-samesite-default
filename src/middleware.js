5f95d15d2808d5d9a758f5575878317457dc07b7
Daniel Brain <dbrain@paypal.com>
2019-11-11 17:19:22 -0800

    Limit SameSite=None cookie to Chrome and Chromium

--- src/middleware.js
+++ src/middleware.js
@@ -6,6 +6,13 @@ type Middleware = (req : ExpressRequest, res : ExpressResponse, next : Function)
 
 export function sameSiteCookieMiddleware() : Middleware {
     return (req, res, next) => {
+        const userAgent = req.get('User-Agent') || '';
+        const isChrome = userAgent.length > 0 && userAgent.match(/Chrom(e|ium)/);
+
+        if (!isChrome) {
+            return next();
+        }
+
         const { cookie } = res;
 
         const setCookieWithSameSite = function (name, value, opts, ...args) : void {
--- test/samesite.test.js
+++ test/samesite.test.js
@@ -8,7 +8,65 @@ test('Should add SameSite to a cookie without it set', () => {
 
     let nextCalled = false;
     const cookies = {};
-    const req = {};
+    const req = {
+        get: (key) => {
+            if (key.toLowerCase() === 'user-agent') {
+                return 'Chrome';
+            }
+
+            throw new Error(`Can not get header: ${ key }`);
+        }
+    };
+
+    const res = {
+        cookie: (name, value, opts) => {
+            cookies[name] = { value, opts };
+        }
+    };
+
+    const next = () => {
+        nextCalled = true;
+    };
+
+    // $FlowFixMe
+    middleware(req, res, next);
+
+    if (!nextCalled) {
+        throw new Error(`Expected next to be called`);
+    }
+
+    res.cookie('foo', 'bar');
+
+    const { value, opts } = cookies.foo;
+
+    if (value !== 'bar') {
+        throw new Error(`Expected cookie value to be 'bar', got '${ value }'`);
+    }
+
+    if (!opts) {
+        throw new Error(`Expected opts to be set with cookie`);
+    }
+
+    if (opts.sameSite !== 'None') {
+        throw new Error(`Expected sameSite cookiue opt to be 'None', got '${ opts.sameSite }'`);
+    }
+});
+
+test('Should add SameSite to a cookie without it set when browser is Chromium', () => {
+
+    const middleware = sameSiteCookieMiddleware();
+
+    let nextCalled = false;
+    const cookies = {};
+    const req = {
+        get: (key) => {
+            if (key.toLowerCase() === 'user-agent') {
+                return 'Chromium';
+            }
+
+            throw new Error(`Can not get header: ${ key }`);
+        }
+    };
 
     const res = {
         cookie: (name, value, opts) => {
@@ -50,7 +108,15 @@ test('Should add SameSite to a cookie with opts passed and without it set', () =
 
     let nextCalled = false;
     const cookies = {};
-    const req = {};
+    const req = {
+        get: (key) => {
+            if (key.toLowerCase() === 'user-agent') {
+                return 'Chrome';
+            }
+
+            throw new Error(`Can not get header: ${ key }`);
+        }
+    };
 
     const res = {
         cookie: (name, value, opts) => {
@@ -100,7 +166,15 @@ test('Should not add SameSite to a cookie with it already set', () => {
 
     let nextCalled = false;
     const cookies = {};
-    const req = {};
+    const req = {
+        get: (key) => {
+            if (key.toLowerCase() === 'user-agent') {
+                return 'Chrome';
+            }
+
+            throw new Error(`Can not get header: ${ key }`);
+        }
+    };
 
     const res = {
         cookie: (name, value, opts) => {
@@ -135,5 +209,51 @@ test('Should not add SameSite to a cookie with it already set', () => {
 
     if (opts.sameSite !== 'Strict') {
         throw new Error(`Expected sameSite cookiue opt to be 'Strict', got '${ opts.sameSite }'`);
+    }
+});
+
+test('Should not add SameSite to a cookie when the browser is not chrome or chromium', () => {
+
+    const middleware = sameSiteCookieMiddleware();
+
+    let nextCalled = false;
+    const cookies = {};
+    const req = {
+        get: (key) => {
+            if (key.toLowerCase() === 'user-agent') {
+                return 'Safari';
+            }
+
+            throw new Error(`Can not get header: ${ key }`);
+        }
+    };
+
+    const res = {
+        cookie: (name, value, opts) => {
+            cookies[name] = { value, opts };
+        }
+    };
+
+    const next = () => {
+        nextCalled = true;
+    };
+
+    // $FlowFixMe
+    middleware(req, res, next);
+
+    if (!nextCalled) {
+        throw new Error(`Expected next to be called`);
+    }
+
+    res.cookie('foo', 'bar');
+
+    const { value, opts = {} } = cookies.foo;
+
+    if (value !== 'bar') {
+        throw new Error(`Expected cookie value to be 'bar', got '${ value }'`);
+    }
+
+    if (opts.sameSite) {
+        throw new Error(`Expected sameSite cookie opt to be blank, got '${ opts.sameSite }'`);
     }
 });
